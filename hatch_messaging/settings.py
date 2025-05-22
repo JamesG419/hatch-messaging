ed047e5b6269b26535688d3d8804a59bc60e5a0e
James Glenister <jglen419@gmail.com>
2025-05-22 18:26:14 -0400

    Adding messaging app

--- hatch_messaging/settings.py
+++ hatch_messaging/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hatch_messaging',  # Register the app
+    'rest_framework',  # Django REST framework
+    'messaging',  
 ]
 
 MIDDLEWARE = [
